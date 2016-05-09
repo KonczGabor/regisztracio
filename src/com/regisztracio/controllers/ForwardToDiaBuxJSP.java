9a02009c00ed1b47d7c0bc606f9b03dd53707f87
Calindyl <caridniss@gmail.com>
2016-05-09 20:13:29 +0200

    1

--- src/com/regisztracio/controllers/ForwardToDiaBuxJSP.java
+++ src/com/regisztracio/controllers/ForwardToDiaBuxJSP.java
@@ -2,6 +2,7 @@ package com.regisztracio.controllers;
 
 import java.io.IOException;
 
+import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,12 +10,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.regisztracio.service.DiagramService;
+
 /**
  * Servlet implementation class ForwardToDiaBux
  */
 @WebServlet("/ForwardToDiaBuxJSP")
 public class ForwardToDiaBuxJSP extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	DiagramService ds;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -30,6 +36,18 @@ public class ForwardToDiaBuxJSP extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/DiaBux.jsp");
 
+		
+		
+		ds.createAllDiagrams();
+		
+		String diaBuxDataResult;
+		
+		diaBuxDataResult = ds.getDiagramBuxData();
+		
+		request.setAttribute("diaBuxDataResult", diaBuxDataResult);
+		
+		ds.deleteDiagramBux();
+		
 		view.forward(request, response);
 	}
 
