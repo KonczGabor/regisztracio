35a40bb9c529e2c95e319764291e379ea39d0d9a
Calindyl <caridniss@gmail.com>
2016-05-09 20:08:09 +0200

    nemtom

--- src/com/regisztracio/service/DiagramService.java
+++ src/com/regisztracio/service/DiagramService.java
@@ -1,10 +1,14 @@
 package com.regisztracio.service;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
+import com.regisztracio.models.BelfoldiJogiSzemely;
 import com.regisztracio.models.Diagram;
 
 /**
@@ -21,21 +25,38 @@ public class DiagramService {
         // TODO Auto-generated constructor stub
     }
     
-    @PersistenceContext(unitName = "diagram")
+    @PersistenceContext(unitName = "regisztracio")
 	private EntityManager em;
-    
-    public void deleteAllDiagrams(){
-    	
-    	
-    }
     
     public void createAllDiagrams(){
     	
     	Diagram diagram = new Diagram();
     	
-    	diagram.setName("táblázat");
-    	diagram.setParams("táblázat");
+    	diagram.setName("BUX");
+    	
+    	diagram.setData("data.addRows([ [ '2016.03.29', 25872 ],[ '2016.04.04', 26438 ], [ '2016.04.05', 26402 ],[ '2016.04.06', 26309 ], [ '2016.04.07', 26440 ],[ '2016.04.08', 26528 ], [ '2016.04.11', 26650 ],[ '2016.04.12', 26365 ] ]);");
+    	
+//    	diagram.setData("[ '2016.04.12', 26365 ];");
+
+    	
+    	em.persist(diagram);
+    }
+    
+    
+    public String getDiagramBuxData(){
+    	
+    	TypedQuery<Diagram> query = em.createQuery("SELECT diagram FROM Diagram diagram" , Diagram.class);
+				
+//    	Diagram query = em.createQuery("SELECT d.data FROM Diagram d WHERE diagram.name='BUX'");
+    
+    	List<Diagram> diagramList = query.getResultList();
+    	
+    	return diagramList.get(0).getData();
+    }
+    
+    public void deleteDiagramBux(){
+    	
+    	em.createQuery("DELETE FROM Diagram " , Diagram.class);
     	
     }
-
 }
